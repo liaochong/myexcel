d831b59a773062fd9ce37947c353500ccc6a2df1
liaochong <liaochong@guahao.com>
2017-12-08 09:00:38 +0800

    initialize commit

---
+++ src/main/java/com/github/liaochong/html2excel/App.java
@@ -0,0 +1,13 @@
+package com.github.liaochong.html2excel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
