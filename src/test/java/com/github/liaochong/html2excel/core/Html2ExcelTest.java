d6ba9ef9f29842ba55643f11de11750f95b4de94
liaochong <liaochong@guahao.com>
2017-12-09 09:53:19 +0800

    add unit test

---
+++ src/test/java/com/github/liaochong/html2excel/core/Html2ExcelTest.java
@@ -0,0 +1,146 @@
+package com.github.liaochong.html2excel.core;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Html2Excel Tester.
+ * 
+ * @author <Authors name>
+ * @since
+ * 
+ *        <pre>
+ * 十二月 9, 2017
+ *        </pre>
+ * 
+ * @version 1.0
+ */
+public class Html2ExcelTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * 
+     * Method: readHtml(File htmlFile)
+     * 
+     */
+    @Test
+    public void testReadHtml() throws Exception {
+        // TODO: Test goes here...
+    }
+
+    /**
+     * 
+     * Method: parse()
+     * 
+     */
+    @Test
+    public void testParse() throws Exception {
+        // TODO: Test goes here...
+    }
+
+    /**
+     * 
+     * Method: processTable(Element table, int index)
+     * 
+     */
+    @Test
+    public void testProcessTable() throws Exception {
+        // TODO: Test goes here...
+        /*
+         * try { Method method = Html2Excel.getClass().getMethod("processTable",
+         * Element.class, int.class); method.setAccessible(true);
+         * method.invoke(<Object>, <Parameters>); } catch(NoSuchMethodException e) { }
+         * catch(IllegalAccessException e) { } catch(InvocationTargetException e) { }
+         */
+    }
+
+    /**
+     * 
+     * Method: getSheet(final Element table, int index)
+     * 
+     */
+    @Test
+    public void testGetSheet() throws Exception {
+        // TODO: Test goes here...
+        /*
+         * try { Method method = Html2Excel.getClass().getMethod("getSheet",
+         * final.class, int.class); method.setAccessible(true); method.invoke(<Object>,
+         * <Parameters>); } catch(NoSuchMethodException e) { }
+         * catch(IllegalAccessException e) { } catch(InvocationTargetException e) { }
+         */
+    }
+
+    /**
+     * 
+     * Method: processTr(Element tr, Tr container)
+     * 
+     */
+    @Test
+    public void testProcessTr() throws Exception {
+        // TODO: Test goes here...
+        /*
+         * try { Method method = Html2Excel.getClass().getMethod("processTr",
+         * Element.class, Tr.class); method.setAccessible(true); method.invoke(<Object>,
+         * <Parameters>); } catch(NoSuchMethodException e) { }
+         * catch(IllegalAccessException e) { } catch(InvocationTargetException e) { }
+         */
+    }
+
+    /**
+     * 
+     * Method: processing(Elements elements, Tr container, boolean isTh)
+     * 
+     */
+    @Test
+    public void testProcessing() throws Exception {
+        // TODO: Test goes here...
+        /*
+         * try { Method method = Html2Excel.getClass().getMethod("processing",
+         * Elements.class, Tr.class, boolean.class); method.setAccessible(true);
+         * method.invoke(<Object>, <Parameters>); } catch(NoSuchMethodException e) { }
+         * catch(IllegalAccessException e) { } catch(InvocationTargetException e) { }
+         */
+    }
+
+    /**
+     * 
+     * Method: adjust()
+     * 
+     */
+    @Test
+    public void testAdjust() throws Exception {
+        // TODO: Test goes here...
+        /*
+         * try { Method method = Html2Excel.getClass().getMethod("adjust");
+         * method.setAccessible(true); method.invoke(<Object>, <Parameters>); }
+         * catch(NoSuchMethodException e) { } catch(IllegalAccessException e) { }
+         * catch(InvocationTargetException e) { }
+         */
+    }
+
+    /**
+     * 
+     * Method: adjust(List
+     * <Td>allTds, Td td)
+     * 
+     */
+    @Test
+    public void testAdjustForAllTdsTd() throws Exception {
+        // TODO: Test goes here...
+        /*
+         * try { Method method = Html2Excel.getClass().getMethod("adjust",
+         * List<Td>.class, Td.class); method.setAccessible(true);
+         * method.invoke(<Object>, <Parameters>); } catch(NoSuchMethodException e) { }
+         * catch(IllegalAccessException e) { } catch(InvocationTargetException e) { }
+         */
+    }
+
+}
