c3a9f0aa9ec666c8a7cb4be4789e397521338310
liaochong <liaochong@guahao.com>
2019-02-03 17:12:01 +0800

    优化自适应宽度参数

--- src/main/java/com/github/liaochong/html2excel/utils/TdUtil.java
+++ src/main/java/com/github/liaochong/html2excel/utils/TdUtil.java
@@ -29,7 +29,9 @@ import java.util.regex.Pattern;
  */
 public final class TdUtil {
 
-    private static Pattern pattern = Pattern.compile("^\\d+$");
+    private static Pattern chineseOrCapitalPattern = Pattern.compile("[\u4e00-\u9fa5|A-Z]");
+
+    private static Pattern digitalPattern = Pattern.compile("^\\d+$");
 
     private static final Cache<String, Integer> SPAN_CACHE = new WeakCache<>();
 
@@ -55,7 +57,7 @@ public final class TdUtil {
     }
 
     public static boolean isSpanValid(String span) {
-        return pattern.matcher(span).find();
+        return digitalPattern.matcher(span).find();
     }
 
     public static int getStringWidth(String s, double shift) {
@@ -64,25 +66,19 @@ public final class TdUtil {
         }
         // 最小为1
         double valueLength = 1;
-        String chinese = "[\u4e00-\u9fa5]";
-        String capital = "[A-Z]";
-        // 获取字段值的长度，如果含中文字符，则每个中文字符长度为2，否则为1
-        double chineseShift = 1.2 + shift;
-        double otherShift = 1 + shift;
+        // 获取字段值的长度，如果含中文字符，则每个中文字符长度为1，否则为0.75
+        double chineseOrCapitalShift = 1 + shift;
+        double otherShift = 0.75 + shift;
         for (int i = 0, size = s.length(); i < size; i++) {
             // 获取一个字符
             String temp = s.substring(i, i + 1);
-            // 判断是否为中文字符或者大写字母
-            if (temp.matches(capital) || temp.matches(chinese)) {
-                // 中文字符长度为1.2
-                valueLength += chineseShift;
+            if (chineseOrCapitalPattern.matcher(temp).find()) {
+                valueLength += chineseOrCapitalShift;
             } else {
-                // 其他字符长度为1
                 valueLength += otherShift;
             }
         }
         // 进位取整
         return (int) Math.ceil(valueLength);
     }
-
 }
