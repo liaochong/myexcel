25317847880f758e2124777d31e0fda43e624a08
liaochong <liaochong@guahao.com>
2019-02-05 12:38:56 +0800

    example优化

--- example/src/main/java/com/github/liaochong/example/controller/FreemarkerExcelBuilderExampleController.java
+++ example/src/main/java/com/github/liaochong/example/controller/FreemarkerExcelBuilderExampleController.java
@@ -1,5 +1,6 @@
 package com.github.liaochong.example.controller;
 
+import com.github.liaochong.example.pojo.Product;
 import com.github.liaochong.html2excel.core.ExcelBuilder;
 import com.github.liaochong.html2excel.core.FreemarkerExcelBuilder;
 import org.apache.commons.codec.CharEncoding;
@@ -30,8 +31,7 @@ public class FreemarkerExcelBuilderExampleController {
     @GetMapping("/freemarker/example")
     public void build(HttpServletResponse response) {
         ExcelBuilder excelBuilder = new FreemarkerExcelBuilder();
-        Map<String, Object> dataMap = new HashMap<>();
-        dataMap.put("data", this.getData());
+        Map<String, Object> dataMap = this.getDataMap();
 
         Workbook workbook = excelBuilder.template("/templates/freemarkerToExcelExample.ftl").build(dataMap);
 
@@ -52,8 +52,7 @@ public class FreemarkerExcelBuilderExampleController {
     @GetMapping("/freemarker/defaultStyle/example")
     public void buildWithDefaultStyle(HttpServletResponse response) {
         ExcelBuilder excelBuilder = new FreemarkerExcelBuilder();
-        Map<String, Object> dataMap = new HashMap<>();
-        dataMap.put("data", this.getData());
+        Map<String, Object> dataMap = this.getDataMap();
 
         Workbook workbook = excelBuilder.template("/templates/freemarkerToExcelExample.ftl").useDefaultStyle().build(dataMap);
 
@@ -66,11 +65,31 @@ public class FreemarkerExcelBuilderExampleController {
         }
     }
 
-    private List<Integer> getData() {
-        List<Integer> data = new ArrayList<>();
+    private Map<String, Object> getDataMap() {
+        Map<String, Object> dataMap = new HashMap<>();
+        dataMap.put("sheetName", "freemarker_excel_example");
+
+        List<String> titles = new ArrayList<>();
+        titles.add("Category");
+        titles.add("Product Name");
+        titles.add("Count");
+        dataMap.put("titles", titles);
+
+        List<Product> data = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            data.add(i);
+            Product product = new Product();
+            if (i % 2 == 0) {
+                product.setCategory("蔬菜");
+                product.setName("小白菜");
+                product.setCount(100);
+            } else {
+                product.setCategory("电子产品");
+                product.setName("ipad");
+                product.setCount(999);
+            }
+            data.add(product);
         }
-        return data;
+        dataMap.put("data", data);
+        return dataMap;
     }
 }
--- example/src/main/resources/templates/freemarkerToExcelExample.ftl
+++ example/src/main/resources/templates/freemarkerToExcelExample.ftl
@@ -9,11 +9,23 @@
 </head>
 <body>
 <table>
-    <caption>freemarker example</caption>
+    <caption>${sheetName}</caption>
+    <thead>
+    <tr>
+        <th colspan="3" style="text-align: center;vertical-align: middle;font-weight: bold;font-size: 14px;">产品介绍</th>
+    </tr>
+    <tr>
+        <#list titles as title>
+            <th>${title}</th>
+        </#list>
+    </tr>
+    </thead>
     <tbody>
     <#list data as item>
         <tr>
-            <td>${item}</td>
+            <td>${item.category}</td>
+            <td>${item.name}</td>
+            <td>${item.count}</td>
         </tr>
     </#list>
     </tbody>
