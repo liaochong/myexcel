275cbea64a6a67f13a51f029090a50058636278d
liaochong <liaochong@guahao.com>
2019-02-05 21:34:08 +0800

    example优化

--- example/src/main/java/com/github/liaochong/example/controller/DefaultStreamExcelBuilderExampleController.java
+++ example/src/main/java/com/github/liaochong/example/controller/DefaultStreamExcelBuilderExampleController.java
@@ -1,8 +1,7 @@
 package com.github.liaochong.example.controller;
 
 import com.github.liaochong.example.pojo.ArtCrowd;
-import com.github.liaochong.html2excel.core.DefaultExcelBuilder;
-import com.github.liaochong.html2excel.core.WorkbookType;
+import com.github.liaochong.html2excel.core.DefaultStreamExcelBuilder;
 import org.apache.commons.codec.CharEncoding;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.stereotype.Controller;
@@ -14,22 +13,33 @@ import java.net.URLEncoder;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 /**
  * @author liaochong
  * @version 1.0
  */
 @Controller
-public class DefaultExcelBuilderExampleController {
+public class DefaultStreamExcelBuilderExampleController {
 
-    @GetMapping("/default/excel/example")
-    public void defaultBuild(HttpServletResponse response) throws Exception {
-        List<ArtCrowd> dataList = this.getDataList();
-        Class<?>[] classes = new Class[]{String.class};
-        Workbook workbook = DefaultExcelBuilder.of(ArtCrowd.class).workbookType(WorkbookType.XLS).build(dataList, classes);
+    @GetMapping("/default/excel/stream/example")
+    public void streamBuild(HttpServletResponse response) throws Exception {
+        DefaultStreamExcelBuilder defaultExcelBuilder = DefaultStreamExcelBuilder.of(ArtCrowd.class).threadPool(Executors.newFixedThreadPool(10)).start();
+
+        List<CompletableFuture> futures = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            CompletableFuture future = CompletableFuture.runAsync(() -> {
+                List<ArtCrowd> dataList = this.getDataList();
+                defaultExcelBuilder.append(dataList);
+            });
+            futures.add(future);
+        }
+        futures.forEach(CompletableFuture::join);
+        Workbook workbook = defaultExcelBuilder.build();
 
         response.setCharacterEncoding(CharEncoding.UTF_8);
-        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("艺术生信息.xls", "UTF-8"));
+        response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode("艺术生信息.xlsx", "UTF-8"));
         try {
             workbook.write(response.getOutputStream());
         } catch (IOException e) {
@@ -62,6 +72,4 @@ public class DefaultExcelBuilderExampleController {
         }
         return dataList;
     }
-
-
 }
