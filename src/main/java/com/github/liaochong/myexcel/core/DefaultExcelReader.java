ed212e9c33b95825d259b047d40d118bb8c2aa28
liaochong <liaochong@guahao.com>
2019-04-12 19:55:50 +0800

    新增读取消费者模式

--- src/main/java/com/github/liaochong/myexcel/core/DefaultExcelReader.java
+++ src/main/java/com/github/liaochong/myexcel/core/DefaultExcelReader.java
@@ -30,6 +30,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.util.Collections;
@@ -39,6 +40,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -96,13 +98,7 @@ public class DefaultExcelReader {
         if (fieldMap.isEmpty()) {
             return Collections.emptyList();
         }
-        Workbook wb;
-        if (StringUtil.isBlank(password)) {
-            wb = WorkbookFactory.create(fileInputStream);
-        } else {
-            wb = WorkbookFactory.create(fileInputStream, password);
-        }
-        Sheet sheet = wb.getSheetAt(sheetIndex);
+        Sheet sheet = getSheetOfInputStream(fileInputStream, password);
         return getDataFromFile(sheet, fieldMap);
     }
 
@@ -118,14 +114,58 @@ public class DefaultExcelReader {
         if (fieldMap.isEmpty()) {
             return Collections.emptyList();
         }
+        Sheet sheet = getSheetOfFile(file, password);
+        return getDataFromFile(sheet, fieldMap);
+    }
+
+    public <T> void readThen(@NonNull InputStream fileInputStream, Consumer<T> consumer) throws Exception {
+        readThen(fileInputStream, null, consumer);
+    }
+
+    public <T> void readThen(@NonNull InputStream fileInputStream, String password, Consumer<T> consumer) throws Exception {
+        Map<Integer, Field> fieldMap = getFieldMap();
+        if (fieldMap.isEmpty()) {
+            return;
+        }
+        Sheet sheet = getSheetOfInputStream(fileInputStream, password);
+        readThenConsume(sheet, fieldMap, consumer);
+    }
+
+
+    public <T> void readThen(@NonNull File file, Consumer<T> consumer) throws Exception {
+        readThen(file, null, consumer);
+    }
+
+    public <T> void readThen(@NonNull File file, String password, Consumer<T> consumer) throws Exception {
+        if (!file.getName().endsWith(".xlsx") && !file.getName().endsWith(".xls")) {
+            throw new IllegalArgumentException("Support only. xls and. xlsx suffix files");
+        }
+        Map<Integer, Field> fieldMap = getFieldMap();
+        if (fieldMap.isEmpty()) {
+            return;
+        }
+        Sheet sheet = getSheetOfFile(file, password);
+        readThenConsume(sheet, fieldMap, consumer);
+    }
+
+    private Sheet getSheetOfInputStream(@NonNull InputStream fileInputStream, String password) throws IOException {
+        Workbook wb;
+        if (StringUtil.isBlank(password)) {
+            wb = WorkbookFactory.create(fileInputStream);
+        } else {
+            wb = WorkbookFactory.create(fileInputStream, password);
+        }
+        return wb.getSheetAt(sheetIndex);
+    }
+
+    private Sheet getSheetOfFile(@NonNull File file, String password) throws IOException {
         Workbook wb;
         if (StringUtil.isBlank(password)) {
             wb = WorkbookFactory.create(file);
         } else {
             wb = WorkbookFactory.create(file, password);
         }
-        Sheet sheet = wb.getSheetAt(sheetIndex);
-        return getDataFromFile(sheet, fieldMap);
+        return wb.getSheetAt(sheetIndex);
     }
 
     private Map<Integer, Field> getFieldMap() {
@@ -150,7 +190,6 @@ public class DefaultExcelReader {
         return fieldMap;
     }
 
-    @SuppressWarnings("unchecked")
     private <T> List<T> getDataFromFile(Sheet sheet, Map<Integer, Field> fieldMap) {
         long startTime = System.currentTimeMillis();
         final int firstRowNum = sheet.getFirstRowNum();
@@ -177,23 +216,7 @@ public class DefaultExcelReader {
                 if (lastColNum < 0) {
                     return null;
                 }
-                T obj;
-                try {
-                    obj = (T) dataType.newInstance();
-                } catch (InstantiationException | IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
-
-                fieldMap.forEach((key, field) -> {
-                    Cell cell = row.getCell(key, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-                    if (Objects.isNull(cell)) {
-                        return;
-                    }
-                    String content = formatter.formatCellValue(cell);
-                    field.setAccessible(true);
-                    ReadConverterContext.convert(content, field, obj);
-                });
-
+                T obj = instanceObj(fieldMap, formatter, row);
                 return new ParallelContainer<>(rowNum, obj);
             }).filter(Objects::nonNull).collect(Collectors.toList());
             log.info("Reading excel takes {} milliseconds", System.currentTimeMillis() - startTime);
@@ -215,25 +238,62 @@ public class DefaultExcelReader {
                 if (lastColNum < 0) {
                     continue;
                 }
-                T obj;
-                try {
-                    obj = (T) dataType.newInstance();
-                } catch (InstantiationException | IllegalAccessException e) {
-                    throw new RuntimeException(e);
-                }
+                T obj = instanceObj(fieldMap, formatter, row);
                 result.add(obj);
-                fieldMap.forEach((key, field) -> {
-                    Cell cell = row.getCell(key, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
-                    if (Objects.isNull(cell)) {
-                        return;
-                    }
-                    String content = formatter.formatCellValue(cell);
-                    field.setAccessible(true);
-                    ReadConverterContext.convert(content, field, obj);
-                });
             }
             log.info("Reading excel takes {} milliseconds", System.currentTimeMillis() - startTime);
             return result;
         }
+    }
+
+    private <T> void readThenConsume(Sheet sheet, Map<Integer, Field> fieldMap, Consumer<T> consumer) {
+        long startTime = System.currentTimeMillis();
+        final int firstRowNum = sheet.getFirstRowNum();
+        final int lastRowNum = sheet.getLastRowNum();
+        log.info("FirstRowNum:{},LastRowNum:{}", firstRowNum, lastRowNum);
+        if (lastRowNum < 0) {
+            log.info("Reading excel takes {} milliseconds", System.currentTimeMillis() - startTime);
+            return;
+        }
+        DataFormatter formatter = new DataFormatter();
+        for (int i = firstRowNum; i <= lastRowNum; i++) {
+            Row row = sheet.getRow(i);
+            if (Objects.isNull(row)) {
+                log.info("Row of {} is null,it will be ignored.", i);
+                continue;
+            }
+            boolean noMatchResult = rowFilter.negate().test(row);
+            if (noMatchResult) {
+                log.info("Row of {} does not meet the filtering criteria, it will be ignored.", i);
+                continue;
+            }
+            int lastColNum = row.getLastCellNum();
+            if (lastColNum < 0) {
+                continue;
+            }
+            T obj = instanceObj(fieldMap, formatter, row);
+            consumer.accept(obj);
+        }
+        log.info("Reading excel takes {} milliseconds", System.currentTimeMillis() - startTime);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T instanceObj(Map<Integer, Field> fieldMap, DataFormatter formatter, Row row) {
+        T obj;
+        try {
+            obj = (T) dataType.newInstance();
+        } catch (InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+        fieldMap.forEach((key, field) -> {
+            Cell cell = row.getCell(key, Row.MissingCellPolicy.RETURN_BLANK_AS_NULL);
+            if (Objects.isNull(cell)) {
+                return;
+            }
+            String content = formatter.formatCellValue(cell);
+            field.setAccessible(true);
+            ReadConverterContext.convert(content, field, obj);
+        });
+        return obj;
     }
 }
