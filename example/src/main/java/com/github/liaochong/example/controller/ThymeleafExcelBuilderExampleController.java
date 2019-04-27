64eeda0bc91d87a292dd6e4780e2f74e3972dff3
liaochong <liaochong@guahao.com>
2019-04-27 17:20:19 +0800

    新增示例

---
+++ example/src/main/java/com/github/liaochong/example/controller/ThymeleafExcelBuilderExampleController.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 liaochong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.liaochong.example.controller;
+
+import com.github.liaochong.myexcel.core.ExcelBuilder;
+import com.github.liaochong.myexcel.core.ThymeleafExcelBuilder;
+import com.github.liaochong.myexcel.utils.AttachmentExportUtil;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * @author liaochong
+ * @version 1.0
+ */
+@RestController
+public class ThymeleafExcelBuilderExampleController {
+
+    @GetMapping("/thymeleaf/example")
+    public void build(HttpServletResponse response) throws IOException {
+        ExcelBuilder excelBuilder = new ThymeleafExcelBuilder();
+
+        Workbook workbook = excelBuilder.template("/templates/demo2.html").build(new HashMap<>());
+        AttachmentExportUtil.export(workbook, "thymeleaf_excel", response);
+    }
+}
