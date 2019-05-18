f34cf2f4dd45989a4c0badddc484bf990fb5491a
liaochong <liaochong@guahao.com>
2019-05-18 13:06:41 +0800

    optimize example

--- example/src/main/java/com/github/liaochong/example/pojo/ArtCrowd.java
+++ example/src/main/java/com/github/liaochong/example/pojo/ArtCrowd.java
@@ -12,16 +12,16 @@ import java.time.LocalDateTime;
 @ExcelTable(sheetName = "艺术生", rowAccessWindowSize = 100, useFieldNameAsTitle = true)
 public class ArtCrowd extends People {
 
-    @ExcelColumn(order = 5, index = 3, width = 20)
+    @ExcelColumn(order = 3, index = 3, width = 20)
     private String paintingLevel;
 
     @ExcelColumn(order = 4, title = "是否会跳舞", width = 9, groups = {People.class, String.class}, index = 4)
     private boolean dance;
 
-    @ExcelColumn(order = 6, title = "考核时间", width = 10, dateFormatPattern = "yyyy-MM-dd HH:mm:ss", groups = {People.class, String.class})
+    @ExcelColumn(order = 5, title = "考核时间", width = 10, dateFormatPattern = "yyyy-MM-dd HH:mm:ss", groups = {People.class, String.class}, index = 5)
     private LocalDateTime assessmentTime;
 
-    @ExcelColumn(order = 7, defaultValue = "---")
+    @ExcelColumn(order = 6, defaultValue = "---")
     private String hobby;
 
     public String getPaintingLevel() {
