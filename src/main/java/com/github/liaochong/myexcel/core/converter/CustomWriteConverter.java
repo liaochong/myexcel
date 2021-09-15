dd3199fdce95493bb2c6c1c9d1300f5090f766f6
liaochong <liaochong@guahao.com>
2021-09-15 22:31:14 +0800

    完善注释

--- src/main/java/com/github/liaochong/myexcel/core/converter/CustomWriteConverter.java
+++ src/main/java/com/github/liaochong/myexcel/core/converter/CustomWriteConverter.java
@@ -10,7 +10,8 @@ public interface CustomWriteConverter<T, F> {
     /**
      * 转化原始数据为指定映射
      *
-     * @param originalData 原始数据
+     * @param originalData       原始数据
+     * @param customWriteContext 自定义写上下文
      * @return 映射
      */
     F convert(T originalData, CustomWriteContext customWriteContext);
